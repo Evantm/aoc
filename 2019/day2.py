df85dc8eb20227e5bae7bc9bb8c8a01ea9a60877
Evan Mackay <etmackay@gmail.com>
2019-12-01 21:46:29 -0800

    cleaned up day 2

--- 2019/day2.py
+++ 2019/day2.py
@@ -11,46 +11,54 @@ def part_one():
 
 			opcode = nums[i]
 
-			if opcode == 1: #addiition
+			if opcode == ADD:
 				nums[nums[i+3]] = nums[nums[i+2]] + nums[nums[i+1]]
 
-			if opcode == 2: 
+			if opcode == MUL: 
 				nums[nums[i+3]] = nums[nums[i+2]] * nums[nums[i+1]]
 
-			if opcode == 99:
+			if opcode == END:
 				break
 
 		print(nums[0])
 
 def part_two():
 
+		with open('day2.input') as file: #hecken slow and lazy way to maintain same nums
+			orginal_nums = eval(file.read())
+			orginal_nums = list(orginal_nums)
+		def get_numbers():
+			return orginal_nums.copy()
 
 		for noun in range(100):
 			for verb in range(100):
-				with open('day2.input') as file: #hecken slow and lazy way to maintain same nums
-					nums = eval(file.read())
-					nums = list(nums)
+				nums = get_numbers()
 					
 
-					nums[1] = noun
-					nums[2] = verb
-					for i in range(0,len(nums),4):
-						# print(nums[i])
+				nums[1] = noun
+				nums[2] = verb
+				for i in range(0,len(nums),4):
+					# print(nums[i])
 
-						opcode = nums[i]
+					opcode = nums[i]
 
-						if opcode == 1: #addiition
-							nums[nums[i+3]] = nums[nums[i+2]] + nums[nums[i+1]]
+					if opcode == ADD: #addiition
+						nums[nums[i+3]] = nums[nums[i+2]] + nums[nums[i+1]]
 
-						if opcode == 2: 
-							nums[nums[i+3]] = nums[nums[i+2]] * nums[nums[i+1]]
+					if opcode == MUL: 
+						nums[nums[i+3]] = nums[nums[i+2]] * nums[nums[i+1]]
 
-						if opcode == 99:
-							break
-
-					if nums[0] == 19690720:
-						print(noun,verb)
+					if opcode == END:
 						break
 
+				if nums[0] == 19690720:
+					print(noun,verb)
+					return
+
 if __name__ == '__main__':
+
+	ADD = 1
+	MUL = 2
+	END = 99
+	part_one()
 	part_two()
