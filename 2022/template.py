fe11520668b55630a7a5d410dc9eaf7c7dc0889d
Evan  ™️ <etmackay@gmail.com>
2022-12-04 21:34:02 -0800

    day5

---
+++ 2022/template.py
@@ -0,0 +1,22 @@
+
+input_lines = open('04.input').read()
+
+sample = '''2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8'''
+
+
+sum_1 = 0
+for line in sample.split('\n'):
+	...
+
+
+
+
+# sum_2 = 0
+# for line in sample.split('\n'):
+# 	...
+# 	
